1fd047d509c30b30ab47241dcdd14b4d892a2d24
Raí <rai.gomes.ismart@gmail.com>
2015-09-23 17:19:18 -0300

    Adiciona detalhes finais.

---
+++ src/main/webapp/js/form.js
@@ -0,0 +1,8 @@
+function mostraPFForm() {
+	document.getElementById("pessoafisica").style.display = "block";
+	document.getElementById("pessoajuridica").style.display = "none";
+}
+function mostraPJForm() {
+	document.getElementById("pessoafisica").style.display = "none";
+	document.getElementById("pessoajuridica").style.display = "block";				
+}
